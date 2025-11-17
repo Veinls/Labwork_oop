2549f3d460a600338232791661f92be4526112b2
Lisa Ulanov <elisaveta_ulan@mail.ru>
2025-11-18 02:56:40 +0300

    Clean lab2 setup complete

--- labvork_1_csharp/src/Models/Teacher.cs
+++
@@ -1,9 +0,0 @@
-namespace labvork_1_csharp.Models
-{
-    public class Teacher : Person
-    {
-        public string Department { get; set; } = string.Empty;
-        
-        public List<Course> Courses { get; set; } = new List<Course>();
-    }
-}
