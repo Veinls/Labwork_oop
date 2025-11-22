7d3352d80da8ba21f612d2cbc6250f419118b637
Lisa Ulanov <elisaveta_ulan@mail.ru>
2025-11-22 06:06:22 +0300

    Очистка ветки lab3 от файлов main

--- labvork_1_csharp/src/Models/Student.cs
+++
@@ -1,9 +0,0 @@
-namespace labvork_1_csharp.Models
-{
-    public class Student : Person
-    {
-        public string Group { get; set; } = string.Empty;
-
-        public List<Course> Courses { get; set; } = new List<Course>();
-    }
-}
--- labvork_1_csharp/src/Services/Interfaces/IRelationshipManager.cs
+++
@@ -1,9 +0,0 @@
-namespace labvork_1_csharp.Services.Interfaces
-{
-    public interface IRelationshipManager<TEntity, TRelatedEntity>
-    {
-        bool AddRelationship(int entityID, int relatedEntityID);
-        bool RemoveRelationship(int entityID, int relatedEntityID);
-        IEnumerable<TRelatedEntity> GetRelatedEntities(int entityID);
-    }
-}
